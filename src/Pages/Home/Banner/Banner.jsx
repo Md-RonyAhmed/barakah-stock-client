44a27eb234f8db50551779465c8c3c3f32e9b2d5
Md. Rony <rony15-10527@diu.edu.bd>
2022-05-04 04:40:41 +0600

    Contact pages updated

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -47,7 +47,7 @@ const Banner = () => {
               className="block w-full md:h-[600px] h-[400px] blur-[1.5px] brightness-90 contrast-75"
               alt=""
             />
-            <div class="carousel-caption block md:block absolute text-center">
+            <div className="carousel-caption block md:block absolute text-center">
               <h5 className="text-3xl md:text-6xl font-bold">
                 Stocks most Beautiful <br />{" "}
                 <span className="text-orange-500">Islamic products</span>
@@ -60,7 +60,7 @@ const Banner = () => {
                 <Link
                   to="/login"
                   type="button"
-                  class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                  className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
                 >
                   Explore Now
                 </Link>
@@ -73,7 +73,7 @@ const Banner = () => {
               className="block w-full md:h-[600px] h-[400px] blur-[1.5px] brightness-90 contrast-75"
               alt="Wild Landscape"
             />
-            <div class="carousel-caption block md:block absolute text-center">
+            <div className="carousel-caption block md:block absolute text-center">
               <h5 className="text-3xl md:text-6xl font-bold">
                 Manage your Inventory <br />{" "}
                 <span className="text-orange-500">After Register here</span>
@@ -86,7 +86,7 @@ const Banner = () => {
                 <Link
                   to="/login"
                   type="button"
-                  class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                  className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
                 >
                   Explore Now
                 </Link>
@@ -99,7 +99,7 @@ const Banner = () => {
               className="block w-full md:h-[600px] h-[400px] blur-[1.5px] brightness-90 contrast-75"
               alt="Camera"
             />
-            <div class="carousel-caption block md:block absolute text-center">
+            <div className="carousel-caption block md:block absolute text-center">
               <h5 className="text-3xl md:text-6xl font-bold">
                 Read latest Blogs About{" "}
                 <span className="text-orange-500">Science & Technology</span>
@@ -112,7 +112,7 @@ const Banner = () => {
                 <Link
                   to="/login"
                   type="button"
-                  class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                  className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
                 >
                   Explore Now
                 </Link>
@@ -125,7 +125,7 @@ const Banner = () => {
               className="block w-full md:h-[600px] h-[400px] blur-[1.5px] brightness-90 contrast-75"
               alt="Exotic Fruits"
             />
-            <div class="carousel-caption block md:block absolute text-center">
+            <div className="carousel-caption block md:block absolute text-center">
               <h5 className="text-3xl md:text-6xl font-bold">
                 Find best Dealers <br />{" "}
                 <span className="text-orange-500">From Local & Abroad</span>
@@ -137,7 +137,7 @@ const Banner = () => {
                 <Link
                   to="/login"
                   type="button"
-                  class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                  className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
                 >
                   Explore Now
                 </Link>
--- src/Pages/Shared/NotFound/NotFound.jsx
+++ src/Pages/Shared/NotFound/NotFound.jsx
@@ -12,7 +12,7 @@ const NotFound = () => {
        <div className="mx-auto w-36">
          <Link to='/'
            type="button"
-           class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+           className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
          >
            Back to Home
          </Link>
