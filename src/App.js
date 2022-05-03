d33862ac3839450c71ba0eafdf857c2df397a862
Md. Rony <rony15-10527@diu.edu.bd>
2022-05-03 12:19:39 +0600

    Home and footer components updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home/Home/Home";
+import Footer from "./Pages/Shared/Footer/Footer";
+import Header from "./Pages/Shared/Header/Header";
 
 function App() {
   return (
     <div>
-     <Home></Home>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home></Home>} ></Route>
+
+      </Routes>
+      <Footer/>
     </div>
   );
 }
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -4,7 +4,7 @@ import Header from '../../Shared/Header/Header';
 const Home = () => {
    return (
       <div>
-         <Header/>
+         <h1>Home</h1>
       </div>
    );
 };
