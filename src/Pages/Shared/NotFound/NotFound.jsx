c363a5593b76265b89dbe90f38ed5804aaba6a54
Md. Rony <rony15-10527@diu.edu.bd>
2022-05-03 15:19:40 +0600

    Not found component completed

--- src/Pages/Shared/NotFound/NotFound.jsx
+++ src/Pages/Shared/NotFound/NotFound.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import gif from '../../../Assets/Images/NotFound/CodePen-404-Page.gif';
 const NotFound = () => {
    return (
-      <div>
-         <h1>Not Found</h1>
-      </div>
+     <div className="mx-auto w-full">
+       <img
+         className="mx-auto w-full md:w-3/4 mb-4"
+         src={gif}
+         alt="not found"
+       />
+       <div className="mx-auto w-36">
+         <Link to='/'
+           type="button"
+           class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+         >
+           Back to Home
+         </Link>
+       </div>
+     </div>
    );
 };
 
