0092c4e66b2a2f531eba1a57b7804877e761eea5
Md. Rony <rony15-10527@diu.edu.bd>
2022-05-05 00:50:09 +0600

    Banner, Home, Subscribe Updated

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Contact from '../Contact/Contact';
+import Subscribe from '../Subscribe/Subscribe';
 
 const Home = () => {
    return (
       <div>
          <Banner/>
-         <Contact/>
+         <Contact />
+         <Subscribe/>
       </div>
    );
 };
