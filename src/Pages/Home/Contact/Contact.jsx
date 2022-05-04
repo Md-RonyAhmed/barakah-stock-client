13e3cf8e95adeae9b30d74c3534363b6c4a8fa2e
Md. Rony <rony15-10527@diu.edu.bd>
2022-05-04 06:04:47 +0600

    Contact and header pages completedd

--- src/Pages/Home/Contact/Contact.jsx
+++ src/Pages/Home/Contact/Contact.jsx
@@ -100,11 +100,11 @@ const Contact = () => {
                       type="checkbox"
                       className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain mr-2 cursor-pointer"
                       id="exampleCheck87"
-                      checked
+                      defaultChecked
                     />
                     <label
                       className="form-check-label inline-block text-gray-800"
-                      for="exampleCheck87"
+                      htmlFor="exampleCheck87"
                     >
                       Send me a copy of this message
                     </label>
