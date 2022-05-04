0530d8975cf3d5b338a70d6e170924fb66a7edf6
Md. Rony <rony15-10527@diu.edu.bd>
2022-05-05 00:04:34 +0600

    Contact Updated

--- src/Pages/Home/Contact/Contact.jsx
+++ src/Pages/Home/Contact/Contact.jsx
@@ -4,7 +4,7 @@ const Contact = () => {
   return (
     <div id="contact" className="container my-7 px-6 mx-auto">
       <div className="border-l-4 mb-6 border-blue-800 p-1 ml-4 pl-2 bg-blue-100">
-        <h1 className="text-4xl font-bold text-blue-700 my-6 font-mono">
+        <h1 className="text-4xl font-bold text-blue-500 my-6 font-mono">
           Contact With Us
         </h1>
       </div>
