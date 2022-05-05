0b02533e81f68bb5ee8cad418cf223273d8511f8
Md. Rony <rony15-10527@diu.edu.bd>
2022-05-05 14:55:28 +0600

    new Blogs added

--- src/Pages/Blogs/Blogs.jsx
+++ src/Pages/Blogs/Blogs.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link} from "react-router-dom";
-import img from '../../Assets/Images/Blog/Nodejs-vs-Javascript-min.webp';
+import img1 from '../../Assets/Images/Blog/Nodejs-vs-Javascript-min.webp';
+import img2 from '../../Assets/Images/Blog/images.png';
 const Blogs = () => {
   return (
     <div className="container my-24 px-6 mx-auto mt-3">
@@ -18,7 +19,7 @@ const Blogs = () => {
               data-mdb-ripple="true"
               data-mdb-ripple-color="light"
             >
-              <img src={img} className="w-full" alt="Louvre" />
+              <img src={img1} className="w-full" alt="Louvre" />
               <a href="#!">
                 <div
                   className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 hover:opacity-100 transition duration-300 ease-in-out"
@@ -69,11 +70,7 @@ const Blogs = () => {
               data-mdb-ripple="true"
               data-mdb-ripple-color="light"
             >
-              <img
-                src="https://mdbootstrap.com/img/new/standard/city/033.jpg"
-                className="w-full"
-                alt="Louvre"
-              />
+              <img src={img2} className="w-full" alt="Louvre" />
               <a href="#!">
                 <div
                   className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 hover:opacity-100 transition duration-300 ease-in-out"
@@ -84,33 +81,34 @@ const Blogs = () => {
           </div>
 
           <div className="grow-0 shrink-0 basis-auto w-full lg:w-6/12 lg:pr-6">
-            <h3 className="text-2xl font-bold mb-4">Exhibition in Paris</h3>
+            <h3 className="text-2xl font-bold mb-4">
+              When should you use Node JS and when should you use mongoDB
+            </h3>
             <div className="text-blue-600 text-sm mb-4 flex items-center font-medium">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 512 512"
-                className="w-4 h-4 mr-2"
-              >
-                <path
-                  fill="currentColor"
-                  d="M204.3 5C104.9 24.4 24.8 104.3 5.2 203.4c-37 187 131.7 326.4 258.8 306.7 41.2-6.4 61.4-54.6 42.5-91.7-23.1-45.4 9.9-98.4 60.9-98.4h79.7c35.8 0 64.8-29.6 64.9-65.3C511.5 97.1 368.1-26.9 204.3 5zM96 320c-17.7 0-32-14.3-32-32s14.3-32 32-32 32 14.3 32 32-14.3 32-32 32zm32-128c-17.7 0-32-14.3-32-32s14.3-32 32-32 32 14.3 32 32-14.3 32-32 32zm128-64c-17.7 0-32-14.3-32-32s14.3-32 32-32 32 14.3 32 32-14.3 32-32 32zm128 64c-17.7 0-32-14.3-32-32s14.3-32 32-32 32 14.3 32 32-14.3 32-32 32z"
-                />
-              </svg>
-              Art
+              Backend Technology
             </div>
-            <p className="text-gray-500">
-              Duis sagittis, turpis in ullamcorper venenatis, ligula nibh porta
-              dui, sit amet rutrum enim massa in ante. Curabitur in justo at
-              lorem laoreet ultricies. Nunc ligula felis, sagittis eget nisi
-              vitae, sodales vestibulum purus. Vestibulum nibh ipsum, rhoncus
-              vel sagittis nec, placerat vel justo. Duis faucibus sapien eget
-              tortor finibus, a eleifend lectus dictum. Cras tempor convallis
-              magna id rhoncus. Suspendisse potenti. Nam mattis faucibus
-              imperdiet. Proin tempor lorem at neque tempus aliquet. Phasellus
-              at ex volutpat, varius arcu id, aliquam lectus. Vestibulum mattis
-              felis quis ex pharetra luctus. Etiam luctus sagittis massa, sed
-              iaculis est vehicula ut.
+            <p className="text-gray-500 mb-4">
+              Nodejs is a Javascript engine that you can write any application
+              you want with (by programming in the Javascript language). It runs
+              your Javascript code. Most commonly, it is used to build servers
+              that can respond to web requests.
             </p>
+            <p className="text-gray-500">
+              MongoDB is a database engine. Code within some application or
+              server uses MongoDB to save, query or update data in a database.
+              <span className="text-blue-500 text-lg underline">
+                click read more...
+              </span>
+            </p>
+            <div className="w-36 mt-2">
+              <Link
+                to="/blogs/details"
+                type="button"
+                className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 uppercase"
+              >
+                Read more...
+              </Link>
+            </div>
           </div>
         </div>
 
