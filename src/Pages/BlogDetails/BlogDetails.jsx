51905407ac8b91f134bb57554436d44894504f9e
Md. Rony <rony15-10527@diu.edu.bd>
2022-05-05 15:38:44 +0600

    new Blogs added

--- src/Pages/BlogDetails/BlogDetails.jsx
+++ src/Pages/BlogDetails/BlogDetails.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import img1 from "../../Assets/Images/Blog/Nodejs-vs-Javascript-min.webp";
 import img2 from "../../Assets/Images/Blog/images.png";
+import img3 from '../../Assets/Images/Blog/images1.jpg';
 const BlogDetails = () => {
    return (
      <div className="container my-16 px-6 mx-auto mt-3">
@@ -139,8 +140,10 @@ const BlogDetails = () => {
          </p>
        </section>
        <section className="mb-32 text-gray-800">
-         <img src={img1} className="w-full shadow-lg rounded-lg mb-6" alt="" />
-
+         <div className="text-yellow-600 text-xl mb-4 flex items-center font-medium">
+           Database Technology
+         </div>
+         <img src={img3} className="w-full shadow-lg rounded-lg mb-6" alt="" />
          <div className="flex items-center mb-6">
            <img
              src="https://mdbootstrap.com/img/Photos/Avatars/img (23).jpg"
@@ -160,40 +163,27 @@ const BlogDetails = () => {
          </div>
 
          <h1 className="font-bold text-3xl mb-6">
-           Differences Between JavaScript vs Node JS
+           Differences between SQL and NoSQL databases
          </h1>
 
          <p className="text-justify">
-           JavaScript is a programming language, which runs in web browsers.
-           Whereas Node.js is an interpreter or running environment for
-           JavaScript, which holds a lot of requiring libraries and all.
-           JavaScript is basically one standard defining programming language;
-           it can run any browser with a default browser running environment. It
-           is a very strong language normally used for a web application on any
-           verification or any specific business logic. We need to append in the
-           screen without page refreshing. JavaScript also helps to use Ajax at
-           any time, which helps us call any server-side script for given
-           dynamic data based on the requirement. It also helps with generating
-           dynamic HTML tables based on business requirement. JQuery is one of
-           the popular libraries to make comfortable use of JavaScript by
-           avoiding to write a lot of code.
+           When choosing a modern database, one of the biggest decisions is
+           picking a relational (SQL) or non-relational (NoSQL) data structure.
+           While both are viable options, there are key differences between the
+           two that users must keep in mind when making a decision. Here, we
+           break down the most important distinctions and discuss the best SQL
+           and NoSQL database systems available.
          </p>
          <p className="mt-3">
-           Node.js also holds a lot of relative libraries, which we normally use
-           in javascript for general purpose programming language. It is
-           actually a kind of environment or interpreter that can represent
-           JavaScript or run any javascript program. It mainly helps us execute
-           some non-blocking operation like some operating system special
-           information like certificate details or hardware details; we may use
-           node js on the same, which help us do it, and JavaScript normal
-           programming will not help us on the same. Normally all browsers have
-           a JavaScript engine that helps us to run javascript in a web browser.
-           Spider monkey (FireFox), JavaScript Core (Safari), V8 (Google Chrome)
-           are some popular javascript engine using verities browsers. But node
-           js is using the V8 engine directly, with some libraries to do some
-           I/O or networking operations. It actually helps us use JavaScript
-           from outside of the browser, like creating, writing or executing one
-           shell script, some back-end services, or running on hardware.
+           The five critical differences between SQL vs NoSQL are: SQL databases
+           are relational, NoSQL databases are non-relational. SQL databases use
+           structured query language and have a predefined schema. NoSQL
+           databases have dynamic schemas for unstructured data. SQL databases
+           are vertically scalable, while NoSQL databases are horizontally
+           scalable. SQL databases are table-based, while NoSQL databases are
+           document, key-value, graph, or wide-column stores. SQL databases are
+           better for multi-row transactions, while NoSQL is better for
+           unstructured data like documents or JSON.
          </p>
        </section>
        <section className="mb-32 text-gray-800">
