72abb884030c31d59f90de44f560495d852fc133
Md. Rony <rony15-10527@diu.edu.bd>
2022-05-06 00:11:30 +0600

    Signin updated

--- src/Pages/Authentication/SignIn/SignIn.jsx
+++ src/Pages/Authentication/SignIn/SignIn.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import img from "../../../Assets/Images/logo/logo.png";
 import SocialLogin from "../SocialLogin/SocialLogin";
 const SignIn = () => {
@@ -6,7 +7,7 @@ const SignIn = () => {
     <div>
       <div className="h-full bg-gradient-to-tl from-green-400 to-indigo-900 w-full py-16 px-4">
         <div className="flex flex-col items-center justify-center w-full">
-          <form className="bg-white shadow rounded lg:w-1/3  md:w-1/2 w-full p-10 mt-16">
+          <form className="bg-white shadow rounded lg:w-1/3  md:w-1/2 w-full h-auto p-10 mt-16">
             <div className="flex items-center justify-center">
               <img
                 className="mr-0"
@@ -27,13 +28,13 @@ const SignIn = () => {
               className="focus:outline-none text-sm mt-4 font-medium leading-none text-gray-500"
             >
               Dont have account?{" "}
-              <a
-                href="dangerouslySetInnerHTML"
-                className="hover:text-gray-500 focus:text-gray-500 focus:outline-none focus:underline hover:underline text-sm font-medium leading-none  text-gray-800 cursor-pointer"
+              <Link
+                to="/signup"
+                className="hover:text-gray-500 focus:text-gray-500 focus:outline-none underline text-sm font-medium leading-none  text-gray-800 cursor-pointer"
               >
                 {" "}
                 Sign up here
-              </a>
+              </Link>
             </p>
             <SocialLogin></SocialLogin>
             <div className="w-full flex items-center justify-between py-5">
@@ -52,8 +53,11 @@ const SignIn = () => {
               </label>
               <input
                 aria-labelledby="email"
+                name="email"
                 type="email"
-                className="bg-gray-200 border rounded  text-xs font-medium leading-none text-gray-800 py-3 w-full pl-3 mt-2"
+                className="bg-gray-100 border rounded text-xs font-medium leading-none text-gray-800 py-3 w-full pl-3 mt-2"
+                placeholder="Enter your email"
+                required
               />
             </div>
             <div className="mt-6 w-full">
@@ -67,7 +71,9 @@ const SignIn = () => {
                 <input
                   id="pass"
                   type="password"
-                  className="bg-gray-200 border rounded  text-xs font-medium leading-none text-gray-800 py-3 w-full pl-3 mt-2"
+                  className="bg-gray-100 border rounded  text-xs font-medium leading-none text-gray-800 py-3 w-full pl-3 mt-2"
+                  placeholder="Enter your password"
+                  required
                 />
                 <div className="absolute right-0 mt-2 mr-3 cursor-pointer">
                   <svg
