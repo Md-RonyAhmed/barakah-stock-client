a0c1733f257537b53bee738196449bab2e3ea037
Md. Rony <rony15-10527@diu.edu.bd>
2022-05-05 06:03:43 +0600

    Blogs updated

--- src/Pages/Blogs/Blogs.jsx
+++ src/Pages/Blogs/Blogs.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import img from '../../Assets/Images/Blog/Nodejs-vs-Javascript-min.webp';
 const Blogs = () => {
   return (
     <div className="container my-24 px-6 mx-auto mt-3">
@@ -17,11 +18,7 @@ const Blogs = () => {
               data-mdb-ripple="true"
               data-mdb-ripple-color="light"
             >
-              <img
-                src="https://mdbootstrap.com/img/new/standard/city/028.jpg"
-                className="w-full"
-                alt="Louvre"
-              />
+              <img src={img} className="w-full" alt="Louvre" />
               <a href="#!">
                 <div
                   className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 hover:opacity-100 transition duration-300 ease-in-out"
@@ -32,31 +29,35 @@ const Blogs = () => {
           </div>
 
           <div className="grow-0 shrink-0 basis-auto w-full lg:w-6/12 lg:pl-6">
-            <h3 className="text-2xl font-bold mb-4">That's the news!</h3>
-            <div className="text-red-600 text-sm mb-4 flex items-center font-medium">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 496 512"
-                className="w-4 h-4 mr-2"
-              >
-                <path
-                  fill="currentColor"
-                  d="M248 8C111.03 8 0 119.03 0 256s111.03 248 248 248 248-111.03 248-248S384.97 8 248 8zm160 215.5v6.93c0 5.87-3.32 11.24-8.57 13.86l-15.39 7.7a15.485 15.485 0 0 1-15.53-.97l-18.21-12.14a15.52 15.52 0 0 0-13.5-1.81l-2.65.88c-9.7 3.23-13.66 14.79-7.99 23.3l13.24 19.86c2.87 4.31 7.71 6.9 12.89 6.9h8.21c8.56 0 15.5 6.94 15.5 15.5v11.34c0 3.35-1.09 6.62-3.1 9.3l-18.74 24.98c-1.42 1.9-2.39 4.1-2.83 6.43l-4.3 22.83c-.62 3.29-2.29 6.29-4.76 8.56a159.608 159.608 0 0 0-25 29.16l-13.03 19.55a27.756 27.756 0 0 1-23.09 12.36c-10.51 0-20.12-5.94-24.82-15.34a78.902 78.902 0 0 1-8.33-35.29V367.5c0-8.56-6.94-15.5-15.5-15.5h-25.88c-14.49 0-28.38-5.76-38.63-16a54.659 54.659 0 0 1-16-38.63v-14.06c0-17.19 8.1-33.38 21.85-43.7l27.58-20.69a54.663 54.663 0 0 1 32.78-10.93h.89c8.48 0 16.85 1.97 24.43 5.77l14.72 7.36c3.68 1.84 7.93 2.14 11.83.84l47.31-15.77c6.33-2.11 10.6-8.03 10.6-14.7 0-8.56-6.94-15.5-15.5-15.5h-10.09c-4.11 0-8.05-1.63-10.96-4.54l-6.92-6.92a15.493 15.493 0 0 0-10.96-4.54H199.5c-8.56 0-15.5-6.94-15.5-15.5v-4.4c0-7.11 4.84-13.31 11.74-15.04l14.45-3.61c3.74-.94 7-3.23 9.14-6.44l8.08-12.11c2.87-4.31 7.71-6.9 12.89-6.9h24.21c8.56 0 15.5-6.94 15.5-15.5v-21.7C359.23 71.63 422.86 131.02 441.93 208H423.5c-8.56 0-15.5 6.94-15.5 15.5z"
-                />
-              </svg>
-              Travels
+            <h3 className="text-2xl font-bold mb-2">
+              Difference between JavaScript and Node JS
+            </h3>
+            <div className="text-red-600 text-sm mb-2 flex items-center font-medium">
+              Web Technology
             </div>
-            <p className="text-gray-500 mb-6">
-              Ut pretium ultricies dignissim. Sed sit amet mi eget urna placerat
-              vulputate. Ut vulputate est non quam dignissim elementum. Donec a
-              ullamcorper diam.
+            <p className="text-gray-500 mb-4">
+              JavaScript is a programming language, which runs in web browsers.
+              Whereas Node.js is an interpreter or running environment for
+              JavaScript, which holds a lot of requiring libraries and all.
             </p>
             <p className="text-gray-500">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quae
-              nulla saepe rerum aspernatur odio amet perferendis tempora
-              mollitia? Ratione unde magni omnis quaerat blanditiis cumque
-              dolore placeat rem dignissimos?
+              Node.js also holds a lot of relative libraries, which we normally
+              use in javascript for general purpose programming language. It is
+              actually a kind of environment or interpreter that can represent
+              JavaScript or run any javascript program.
+              <span className="text-blue-500 text-lg underline">
+                click read more...
+              </span>
             </p>
+            <div className="w-36 mt-2">
+              <Link
+                to="/login"
+                type="button"
+                className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 uppercase"
+              >
+                Read more...
+              </Link>
+            </div>
           </div>
         </div>
 
