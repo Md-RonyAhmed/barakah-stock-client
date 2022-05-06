03d51ddaaa92fc42bd58c55c2991fc9828e46218
Md. Rony <rony15-10527@diu.edu.bd>
2022-05-06 06:52:54 +0600

    Custom link added

---
+++ src/Pages/CustomLink/CustomLink.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
+export const CustomLink = ({ children, to, ...props }) => {
+  let resolved = useResolvedPath(to);
+  let match = useMatch({ path: resolved.pathname, end: true });
+
+  return (
+    <div>
+      <Link
+        style={{
+          background: match ? "black" : "",
+          color: match ? "orange" : "",
+        }}
+        to={to}
+        {...props}
+      >
+        {children}
+      </Link>
+      {match && ""}
+    </div>
+  );
+};
